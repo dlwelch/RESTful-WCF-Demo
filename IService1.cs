756dc950771257940d6ce8560462882efe7489f0
dlwelch <github@dlwelch.com>
2014-10-22 22:50:38 -0500

    Initial Source

---
+++ IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DemoWCFService
+{
+
+   [ServiceContract]
+   
+   public interface IService1
+   {
+
+      [OperationContract]
+      [XmlSerializerFormat]
+      [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Xml,
+          UriTemplate = "artists/{id}")]
+      Artist GetArtist(string id);
+
+      [XmlSerializerFormat]
+      [WebInvoke(Method = "POST", 
+           UriTemplate = "artists")]
+      Artist TestPost(Stream stream);
+   }
+
+
+   [XmlRoot(ElementName = "Artist")]
+   public class Artist
+   {
+      [XmlAttribute("uri")]
+      public string theUri { get; set; }
+
+      [XmlAttribute("rel")]
+      public string theRel { get; set; }
+
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string Grammys { get; set; }
+
+      public Artist() { }
+
+      public Artist(int artistid, string firstName, string lastName, string grammys)
+      {
+         this.theRel = "edit";
+         this.theUri = "/artists/" + artistid;
+         this.FirstName = firstName;
+         this.LastName = lastName;
+         this.Grammys = grammys;
+      }
+   }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.ServiceModel.Activation;
+using System.IO;
+using System.Net;
+
+namespace DemoWCFService
+{
+   // the next line is added for no .svc URIs
+   [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+   public class Service1 : IService1
+   {
+      public Artist GetArtist(string id)
+      {
+         Artist player = new Artist(Convert.ToInt32(id), "Shirley", "Horn", "9");
+         return player;
+      }
+
+      public Artist TestPost(Stream stream)
+      {
+          StreamReader reader = new StreamReader(stream);
+          string data = reader.ReadToEnd();
+          char[] delimiterChars = { '&' };
+          string[] words = data.Split(delimiterChars);
+
+          Artist player = new Artist(1000, words[0].Substring(words[0].IndexOf("=") + 1), 
+              words[1].Substring(words[1].IndexOf("=") + 1),
+              words[2].Substring(words[2].IndexOf("=") + 1));
+          return player;
+       
+      }
+
+   }
+}
